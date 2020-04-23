8e6a900d2c64e81e01ef16ebce25397df0374de2
Francimar <francimarcarlosrn@gmail.com>
2020-04-23 15:00:25 -0300

    Adicionando ListItem

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -2,9 +2,27 @@ import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { ListItem } from 'react-native-elements';
+
+
 import { contatosUsuarioFetch } from '../actions/AppActions';
 
+const list = [
+    {
+        title: 'Registro 1',
+        icon: 'av-timer'
+    },
+    {
+        title: 'Registro 2',
+        icon: 'flight-takeoff'
+    },
+    {
+        title: 'Registro 3'
+    },
+];
+
 class Contatos extends Component {
+    
 
     UNSAFE_componentWillMount(){
         this.props.contatosUsuarioFetch();
@@ -13,8 +31,19 @@ class Contatos extends Component {
     render() {
         return (
             <View>
-                <Text>Contatos</Text>
+                {
+                    list.map((item, i) => (
+                        <ListItem 
+                            key={i}
+                            title={item.title}
+                            leftIcon={{name: item.icon }}
+                            bottomDivider
+                            chevron
+                        />
+                    ))
+                }
             </View>
+
         )
     }
 }
